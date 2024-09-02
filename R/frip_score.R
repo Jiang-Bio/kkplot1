fe71cb15de6ae3f4fc6108b135f26247beb17011
Jiang-Bio <jiangdk_bio@163.com>
2024-09-02 19:03:12 +0800

    frip_score
    frip_score

---
+++ R/frip_score.R
@@ -0,0 +1,27 @@
+#' Title
+#'
+#' @param reads bamfile
+#' @param peaks narrowPeal
+frip <- function(reads, peaks, singleEnd=T) {
+  reads <- BamFile(reads)
+  # find reads in peaks
+  overlaps <- summarizeOverlaps(
+    peaks,
+    reads,
+    mode="IntersectionNotEmpty",
+    ignore.strand=T,
+    singleEnd=singleEnd,
+    count.mapped.reads=T
+  )
+
+  # sum up all reads in peaks and divide by all mapped reads to get FRiP
+  readsInPeaks <- sum(assay(overlaps))
+  if (class(reads) %in% c("BamViews", "BamFile")) {
+    allReads <- colData(overlaps)$mapped
+  } else {
+    allReads <- colData(overlaps)$records
+  }
+  result <- readsInPeaks/allReads
+
+  return(result)
+}
